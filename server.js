286daf52f7948bad5bde5b7d59fe8c5025207b7f
markn <markn@aesclever.com>
2022-06-26 17:55:39 -0700

    update

--- server.js
+++ server.js
@@ -4,18 +4,69 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
+const ObjectID = require('mongodb').ObjectID;
+
+// setting up pug
+app.set("view engine", "pug");
+app.use("views", express.static(process.cwd() + "/views"));
+
+// setting up session and passport
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Default route
+  app.route('/').get((req, res) => {
+    //The response to render the Pug template
+    res.render(process.cwd() + '/views/index', {
+      title: 'Connected to Database',
+      saywoot: 'There are 86400 seconds in a day, and it goes fast fast fast.',
+      message: 'Please login'
+    });
+  });
+  
+  
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      
+      done(null, doc._id);
+    });
+  });
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
